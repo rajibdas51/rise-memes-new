4a3ec3fdc241b0f4888a17445b2623f5755af2a6
Rajib Chandra Das <prorajib6@gmail.com>
2024-12-11 20:32:13 +0600

    modify roadmap component

--- app/components/roadmap/index.tsx
+++ app/components/roadmap/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Image from "next/image";
-import img from "/public/assets/roadmap.jpg";
-import img2 from "/public/assets/roadmap_mobile.jpg";
+import React from 'react';
+import Image from 'next/image';
+import img from '/public/assets/roadmap/roadmap-desktop.png';
+import img2 from '/public/assets/roadmap/roadmap-mobile.png';
 
 const Roadmap = () => {
   return (
     <>
-      <div className="w-full lg:px-20 px-2 py-10">
-        <div className="">
+      <div className='w-full lg:px-20 px-2 py-10'>
+        <div className=''>
           <h1
             className={`lg:text-5xl  font-avon relative text-3xl py-10 uppercase font-[400] text-center text-transparent bg-clip-text bg-gradient-to-b from-[#E4D9BD] to-[#AA8246]  `}
           >
@@ -15,14 +15,19 @@ const Roadmap = () => {
           </h1>
         </div>
 
-        <div className=" flex justify-center h-full w-full ">
+        <div className=' flex justify-center h-full w-full '>
           <Image
             src={img}
-            alt=""
-            loading="lazy"
-            className="md:block hidden w-full max-w-[1660px] rounded-2xl"
+            alt=''
+            loading='lazy'
+            className='md:block hidden w-full max-w-[1660px] rounded-2xl'
           />
-          <Image src={img2} alt="" loading="lazy" className=" md:hidden block w-[90%] rounded-2xl" />
+          <Image
+            src={img2}
+            alt=''
+            loading='lazy'
+            className=' md:hidden block w-[90%] rounded-2xl'
+          />
         </div>
       </div>
     </>
