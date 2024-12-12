75c0f35af565b4fc51f97165d82aad9be6478446
Rajib Chandra Das <prorajib6@gmail.com>
2024-12-13 00:10:06 +0600

    text modification

--- app/components/faqs/data.ts
+++ app/components/faqs/data.ts
@@ -1,88 +1,86 @@
-
 export const faqData = [
   {
-    question: "What is The Rise of Memes?",
+    question: 'What is The Rise of Memes?',
     answer:
-      "The Rise of Memes is a P2E (Play-to-Earn) strategy/RPG mobile game set in a world inspired by crypto memes. Players can build, strategize, develop technologies and battle in real-time using meme-based heroes leading their armies while earning rewards in our native token.",
+      'The Rise of Memes is a P2E (Play-to-Earn) strategy/RPG mobile game set in a world inspired by crypto memes. Players can build, strategize, develop technologies and battle in real-time using meme-based heroes leading their armies while earning rewards in our native token.',
   },
   {
-    question: "What is $RISE?",
+    question: 'What is $RISE?',
     answer:
-      "$RISE is the native token of The Rise of Memes mobile strategy/RPG game. It is used in-game as part of the game economy and rewards system to incentivize players for playing the game and completing challenges.",
+      '$RISE is the native token of The Rise of Memes mobile strategy/RPG game. It is used in-game as part of the game economy and rewards system to incentivize players for playing the game and completing challenges.',
   },
   {
-    question: "What is a token presale?",
+    question: 'What is a token presale?',
     answer:
-      "A token presale is an early opportunity for investors to purchase tokens at a discounted price before the public launch. This helps fund the project while giving early supporters potential gains as the project develops.",
+      'A token presale is an early opportunity for investors to purchase tokens at a discounted price before the public launch. This helps fund the project while giving early supporters potential gains as the project develops.',
   },
   {
-    question: "When does the presale start?",
+    question: 'When does the presale start?',
     answer:
-      "The presale date will be announced in our official X and Telegram channels. Links to the channels can be found on the official website: riseofmemes.com",
+      'The presale date will be announced in our official X and Telegram channels. Links to the channels can be found on the official website: riseofmemes.com',
   },
   {
-    question: "What is the price of the token during the presale?",
+    question: 'What is the price of the token during the presale?',
     answer:
-      "The starting price of $RISE tokens during presale will be $0.0002 per token and will increase gradually until all presale tokens are sold.",
+      'The starting price of $RISE tokens during presale will be $0.0002 per token and will increase gradually until all presale tokens are sold.',
   },
   {
-    question: "What is the total supply of tokens?",
-    answer: "The total supply of $RISE tokens is 100,000,000,000.",
+    question: 'What is the total supply of tokens?',
+    answer: 'The total supply of $RISE tokens is 100,000,000,000.',
   },
   {
-    question: "How much of the total token supply will be sold in the presale?",
+    question: 'How much of the total token supply will be sold in the presale?',
     answer:
-      "20% of the total supply is allocated for the presale, or 20,000,000,000 tokens.",
+      '20% of the total supply is allocated for the presale, or 20,000,000,000 tokens.',
   },
   {
-    question: "How can I participate in the token presale?",
+    question: 'How can I participate in the token presale?',
     answer:
-      "To participate in the presale you will need a DeFi crypto wallet supporting Ethereum or Solana chains. Then you can follow the steps set in our 'How to buy guide' on the official website of The Rise of Meme: riseofmemes.com/howtobuy",
+      'To participate in the presale you will need a DeFi crypto wallet supporting Ethereum. Then you can follow the steps set in our “How to buy guide” on the official website of The Rise of Meme: riseofmemes.com/howtobuy',
   },
   {
-    question: "How will the funds raised during the presale be used?",
+    question: 'How will the funds raised during the presale be used?',
     answer:
-      "Raised funds will be used to add liquidity to the $RISE token, for further game development, marketing purposes and operational costs to grow the project.",
+      'Raised funds will be used to add liquidity to the $RISE token, for further game development, marketing purposes and operational costs to grow the project.',
   },
   {
-    question: "Will there be any incentives to buy more tokens during presale?",
+    question: 'Will there be any incentives to buy more tokens during presale?',
     answer:
-      "Yes. The incentives are laid out in our Game Editions section, where you can enjoy various game bonuses, real-world collector’s edition game statues, additional tokens and more. Find more here: riseofmemes.com/gameeditions",
+      'Yes. The incentives are laid out in our Game Editions section, where you can enjoy various game bonuses, real-world collector’s edition game statues, additional tokens and more. Find more here: riseofmemes.com/gameeditions',
   },
   {
-    question: "When will the token be launched for public trading?",
+    question: 'When will the token be launched for public trading?',
     answer:
-      "The token is expected to launch on public exchanges shortly after the presale concludes. Stay tuned for announcements on the exact launch date.",
+      'The token is expected to launch on public exchanges shortly after the presale concludes. Stay tuned for announcements on the exact launch date.',
   },
   {
     question:
-      "Will there be any vesting or lock-up period for presale participants?",
+      'Will there be any vesting or lock-up period for presale participants?',
     answer:
-      "Presale participants are not subject to a vesting period. The earliest contributors of the project, such as seed investors and developers are subject to a 2-year vesting period.",
+      'Presale participants are not subject to a vesting period. The earliest contributors of the project, such as seed investors and developers are subject to a 2-year vesting period.',
   },
   {
-    question: "What wallets can I use to store my tokens?",
+    question: 'What wallets can I use to store my tokens?',
     answer:
-      "To claim your $RISE tokens after presale you will need a wallet that supports the Solana network, such as the Phantom wallet or Trust wallet.",
+      'To claim your $RISE tokens you will need a wallet that supports Ethereum chain, such as Best wallet of Trust wallet.',
   },
   {
     question: "What makes 'The Rise of Memes' unique in the gaming space?",
     answer:
-      "The Rise of Memes combines the viral culture of crypto memes with strategic, immersive and social gameplay. By participating, players can earn real-world rewards through engaging gameplay, socialization, tokenized assets, and a strong community.",
+      'The Rise of Memes combines the viral culture of crypto memes with strategic, immersive and social gameplay. By participating, players can earn real-world rewards through engaging gameplay, socialization, tokenized assets, and a strong community.',
   },
   {
-    question: "What risks should I consider before purchasing tokens?",
+    question: 'What risks should I consider before purchasing tokens?',
     answer:
-      "Like any investment in cryptocurrency, there are risks involved, including market volatility, regulatory uncertainties, and project-specific risks. We encourage all participants to do their own research and invest responsibly.",
+      'Like any investment in cryptocurrency, there are risks involved, including market volatility, regulatory uncertainties, and project-specific risks. We encourage all participants to do their own research and invest responsibly.',
   },
   {
-    question: "Where can I find more information about the project?",
+    question: 'Where can I find more information about the project?',
     answer:
-      "You can find more detailed information in our whitepaper and by following our official social media channels for updates.",
+      'You can find more detailed information in our whitepaper and by following our official social media channels for updates.',
   },
   {
-    question: "How will I receive the statue?",
-    answer:
-      "",
+    question: 'How will I receive the statue?',
+    answer: ' ',
   },
 ];
--- app/components/how-to-buy/index.tsx
+++ app/components/how-to-buy/index.tsx
@@ -1,60 +1,60 @@
-"use client";
+'use client';
 
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "./how-to-buy.css";
-import TitleHeading from "../common/title-heading";
-import Card from "./card";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './how-to-buy.css';
+import TitleHeading from '../common/title-heading';
+import Card from './card';
 
 export default function HowToBuy() {
   const slides = [
     {
-      title: "Connect wallet",
+      title: 'Connect wallet',
       description:
-        "Install a Defi wallet such as Metamask, Best Wallet, Trust Wallet for ETH purchase or Phantom wallet if you wish to buy with Solana. Connect it to our secure presale widget to buy and track your $RISE token holdings.",
-      subtitle: "1",
+        'Install a Defi wallet such as Best Wallet or Trust Wallet for ETH or USDT ERC-20 purchase. Connect it to our secure presale widget to buy and track your $RISE token holdings.',
+      subtitle: '1',
     },
     {
-      title: "Have ETH, USDT or SOL",
+      title: 'Have ETH or USDT ERC-20',
       description:
-        "To purchase $RISE tokens you need to have Ethereum (ETH), Tether ERC-20 (USDT) or Solana (SOL) on your wallet. Deposit crypto to your wallet or purchase crypto directly in your wallet.",
-      subtitle: "2",
+        'To purchase $RISE tokens you need to have Ethereum (ETH), Tether ERC-20 (USDT) in your wallet. Deposit crypto to your wallet or purchase crypto directly in your wallet. ',
+      subtitle: '2',
     },
     {
-      title: "Swap",
+      title: 'Swap',
       description:
-        "Now you can swap your ETH, USDT or SOL for $RISE tokens. Select how much you would like to spend and you will see how many tokens you will receive. Make sure you leave a little ETH or SOL for gas fees (network transaction fee).",
-      subtitle: "3",
+        'Now you can swap your ETH or USDT for $RISE tokens. Select how much you would like to spend and you will see how many tokens you will receive. Make sure you leave a little ETH for gas fees (network transaction fee). ',
+      subtitle: '3',
     },
     {
-      title: "Track and Claim",
+      title: 'Track and Claim',
       description:
-        "After you successfully purchase $RISE tokens, you will be able to track how many tokens your wallet has when connected to the presale widget. Staking will be available shortly after presale launch. You will be able to stake your tokens for additional rewards or simply hold them until the presale is concluded and you will be able to claim your tokens.",
-      subtitle: "4",
+        'After you successfully purchase $RISE tokens, you will be able to track how many tokens your wallet has when connected to the presale widget. Staking will be available shortly after presale launch. You will be able to stake your tokens for additional rewards or simply hold them until the presale is concluded and you will be able to claim your tokens. ',
+      subtitle: '4',
     },
   ];
 
   return (
-    <div className="carousel-container pt-20 lg:pb-48 pb-28 px-4 sm:px-0">
+    <div className='carousel-container pt-20 lg:pb-48 pb-28 px-4 sm:px-0'>
       <TitleHeading>HOW TO BUY</TitleHeading>
 
-      <div className="w-full flex justify-center items-center relative mt-5 lg:mt-0">
-        <div className="swiper-button-prev" id="prevButton"></div>
-        <div className="swiper-button-next" id="nextButton"></div>
+      <div className='w-full flex justify-center items-center relative mt-5 lg:mt-0'>
+        <div className='swiper-button-prev' id='prevButton'></div>
+        <div className='swiper-button-next' id='nextButton'></div>
 
         <Swiper
-          effect={"coverflow"}
+          effect={'coverflow'}
           grabCursor={true}
           centeredSlides={true}
           loop={true}
           navigation={{
-            prevEl: "#prevButton",
-            nextEl: "#nextButton",
+            prevEl: '#prevButton',
+            nextEl: '#nextButton',
           }}
           coverflowEffect={{
             rotate: 0,
@@ -64,7 +64,7 @@ export default function HowToBuy() {
             slideShadows: false,
           }}
           modules={[EffectCoverflow, Pagination, Navigation]}
-          className="mySwiper"
+          className='mySwiper'
           breakpoints={{
             0: {
               slidesPerView: 1,
