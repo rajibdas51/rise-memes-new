ba85ce52876763fa634dcfd8657f05e3753cd19d
Rajib Chandra Das <prorajib6@gmail.com>
2024-12-14 00:18:47 +0600

    add terms and conditions page

--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -1,7 +1,204 @@
 import React from 'react';
+import Section from '../components/common/section';
+import bg from '/public/assets/Tokenomics-bg.png';
 
 const TermsAndConditionsPage = () => {
-  return <div>TermsAndConditionsPage</div>;
+  return (
+    <div
+      className='bg-cover bg-center min-h-screen bg-no-repeat lazy-bg'
+      style={{ backgroundImage: `url(${bg.src})` }}
+    >
+      <Section className='pt-16 md:pt-20 flex flex-col'>
+        <h2 className='font-avon text-gradient text-4xl uppercase mb-4 text-left md:text-left'>
+          Terms of Service
+        </h2>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          {`  The following Terms of Service ("Terms") govern your access to and use
+          of the TheRiseOfMemes website (www.riseofmemes.com) and any related
+          services provided by Rise ("us", "we", or "our"). By accessing and
+          using the website and/or participating in our play-to-earn game on the
+          Solana blockchain, you agree to comply with and be bound by these
+          Terms. If you do not agree with these Terms, you must refrain from
+          accessing or using the website.`}
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          These Terms, along with the Privacy Policy and Cookies Policy
+          available on the website, constitute your legal agreement with Rise.
+          You agree not to assign any rights or obligations under these Terms
+          without our prior written consent. We may assign our rights and
+          obligations under these Terms at our sole discretion to an affiliate,
+          subcontractor, offshore company, or in connection with an acquisition,
+          sale, or merger.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          If any paragraphs or clauses contained in these Terms become
+          unenforceable or invalid, this will not affect the applicability of
+          any other parts of these Terms and/or the validity of this entire
+          policy.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          If any paragraphs or clauses contained in these Terms become
+          unenforceable or invalid, this will not affect the applicability of
+          any other parts of these Terms and/or the validity of this entire
+          policy.
+        </p>
+
+        <h2 className='pt-6  font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          About Ortu Entertainment{' '}
+        </h2>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          Ortu Entertainment is the parent company behind the Rise project,
+          dedicated to creating innovative and engaging experiences in the
+          blockchain gaming space. Our mission is to develop entertaining
+          platforms that not only provide users with immersive gameplay but also
+          empower them through the unique opportunities presented by
+          cryptocurrency and blockchain technology. As we continue to grow, we
+          strive to uphold our commitment to transparency, community engagement,
+          and the highest standards of service for all our users.
+        </p>
+        <h2 className='pt-6  font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          Acceptance of Terms
+        </h2>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          {`  By accessing or using www.riseofmemes.com, you acknowledge that you
+          have read, understood, and agree to abide by these Terms. These Terms
+          may be changed at any time, and you should check the "Last Updated"
+          date for any changes. By continuing to use the website and/or
+          purchasing, acquiring, or being in possession of the $RISE token after
+          the updated Terms, you inherently agree to the modifications of these
+          Terms.`}
+        </p>
+        <h2 className='pt-6  font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          Definitions{' '}
+        </h2>
+        <ul className='list-disc pl-5   md:pl-10 py-3'>
+          <li className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+            {`"TheRiseOfMemes" refers to the cryptocurrency project developed with
+            the objectives defined by its creators.`}
+          </li>
+          <li className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+            {`"TheRiseOfMemes" can also refer to the company; when this is the case, the word "company" will be included to avoid confusion.`}
+          </li>
+          <li className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+            {`"$RISE" refers to the digital cryptocurrency token associated with the Rise project.`}
+          </li>
+          <li className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+            {`"User" or "You" refers to any individual or entity that purchases, uses, or interacts with either the website www.riseofmemes.com or the token $RISE.`}
+          </li>
+        </ul>
+        <h2 className='pt-6  font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          Eligibility{' '}
+        </h2>
+
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          You must be at least 18 years old to use our platform. By using
+          www.riseofmemes.com, you confirm that you are of legal age in your
+          jurisdiction. If any minor engages with www.riseofmemes.com and/or
+          buys the $RISE token, it is considered that they do so with the
+          support and consent of their parent or guardian, and therefore, we are
+          not responsible for their activities.
+        </p>
+        <h2 className='pt-6 font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          Intellectual Property Rights
+        </h2>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          a. By accessing www.riseofmemes.com, you are granted a limited,
+          non-sublicensable license to access and use our services and token
+          subject to these Terms.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          b. You agree that any information and media, including logos,
+          trademarks, and text, are protected by intellectual property laws, and
+          you cannot exercise any rights regarding these without our permission.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          c. You may not modify any of our products or services nor create any
+          derivatives associated with these without our written permission.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          d. You will not attempt to receive any financial gains from
+          distributing, sharing, or promoting our services and any content
+          related to them without a written agreement with us. If you provide us
+          with suggestions or feedback, endorse, or promote our token and
+          services on your personal social accounts or external websites, you do
+          so voluntarily and are not expected to be compensated or given any
+          ownership for those.
+        </p>
+        <h2 className='pt-6  font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          Cryptocurrency Risks
+        </h2>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          a. Investment Risk: Purchasing cryptocurrencies, including $RISE,
+          carries inherent risks. Prices can be highly volatile, and you may
+          lose all or part of your investment. We do not provide investment
+          advice, and you are solely responsible for your investment decisions.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          b. Security: You are responsible for maintaining the security of your
+          wallet and private keys. TheRiseOfMemes is not liable for any losses
+          or damages resulting from your failure to secure your assets properly.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          c. Regulatory Compliance: Cryptocurrency regulations vary by
+          jurisdiction. It is your responsibility to comply with the laws and
+          regulations in your area regarding cryptocurrency use.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          d. No Guarantees: Rise makes no guarantees or warranties about the
+          value, stability, or future performance of the $RISE token.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          e. Claim of Tokens: Claiming the $RISE tokens after the end of the
+          presale can only be done with the same wallet from which they were
+          purchased in the presale. In the event of losing access or the
+          compromise of the wallet, you agree that we are not responsible for
+          your loss, nor can we allow you to claim from a different wallet since
+          we do not undergo any KYC procedures when enabling purchases during
+          the presale. The only exception to this is if the transaction has been
+          completed via a verified third-party service, in which case you will
+          need to contact us to verify your identity.
+        </p>
+
+        <h2 className='pt-6  font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          Limitation of Liability
+        </h2>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          {`TheRiseOfMemes, its developers, employees, and associated entities
+          will not be held liable for any damages, losses, or expenses resulting
+          from your use or inability to use www.riseofmemes.com or from any
+          unauthorized access to or use of our servers and/or any information
+          stored therein. www.riseofmemes.com is provided "as is" and "as
+          available" without any warranties, expressed or implied, for any
+          particular purpose. Although we aim to offer a secure and reliable
+          service, we cannot guarantee that www.riseofmemes.com will always be
+          secure, timely, and free of bugs. Some services might be suspended
+          temporarily due to technical or maintenance issues. We may also change
+          or suspend parts of the services at our sole discretion.`}
+        </p>
+        <h2 className='pt-6 font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          Third-Party Content
+        </h2>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          {`www.riseofmemes.com might display content and/or links to third-party websites. You agree that we are not responsible for any activity developed by third-party websites, affiliates, or companies. You should inform yourself about their terms of use and privacy policies before accessing their services.`}
+        </p>
+        <h2 className='pt-6  font-avon text-gradient text-2xl uppercase mb-4 text-left md:text-left'>
+          Contact Information
+        </h2>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          If you have any questions or concerns regarding these Terms or the
+          www.riseofmemes.com website, you can contact us at
+          contact@riseofmemes.com.
+        </p>
+        <p className='text-[#fff1de] text-shadow-effect-para font-[500] font-cinzel text-sm md:text-lg text-left md:text-left no-scrollbar'>
+          By using www.riseofmemes.com, you acknowledge that you have read and
+          understood these Terms of Service and agree to be bound by them. These
+          Terms, along with the Privacy and Cookies Policy, constitute the
+          entire agreement between you and Rise regarding your use of the
+          platform.
+        </p>
+      </Section>
+    </div>
+  );
 };
 
 export default TermsAndConditionsPage;
