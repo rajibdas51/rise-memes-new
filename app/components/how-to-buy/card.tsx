fe22c80f04eab1c9f5857610141a3cab5482632c
Rajib Chandra Das <prorajib6@gmail.com>
2024-12-18 22:06:26 +0600

    fix how to buy section

--- app/components/how-to-buy/card.tsx
+++ app/components/how-to-buy/card.tsx
@@ -11,14 +11,15 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, description, subtitle }) => {
   return (
     <div
-      className='relative lg:px-5 px-3 md:px-2 backdrop-blur-[3px] lg:max-w-[538px] md:h-[322px] md:max-w-[345px] h-[230px] overflow-hidden flex flex-col justify-center items-center lg:gap-4 gap-1 bg-contain   bg-center lazy-bg w-full bg-no-repeat pb-1'
+      className='buy-container relative lg:px-5 px-3 md:px-2 backdrop-blur-[3px] lg:max-w-[538px] md:h-[322px] md:max-w-[345px] h-[280px] overflow-hidden flex flex-col justify-center items-center lg:gap-4 gap-1 !bg-contain md:bg-cover z-50   md:pt-0  bg-center lazy-bg w-[93vw] bg-no-repeat pb-1'
       data-bg='/assets/buyframe.png'
+      style={{ backgroundSize: 'contain!' }}
     >
       <h1 className='lg:text-[28.36px] text-[20px] mt-4 lg:leading-[46px] leading-[30px] font-bold bg-gradient-to-b from-[#E4D9BD] to-[#AA8246] bg-clip-text text-transparent text-center'>
         {title}
       </h1>
       <p
-        className='text-center text-[#FFF1DE] mt-0 pt-3 mb-1 text-wrap  font-medium 
+        className='text-center text-[#FFF1DE] mt-0 pt-4 px-1 mb-1 text-wrap  font-medium 
              text-[14px] lg:leading-[21px] leading-[18px]
              lg:text-[17px]   
       '
