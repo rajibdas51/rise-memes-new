7bf480b7e36383f3bbe7a05ecde0a8c254157a00
Rajib Chandra Das <prorajib6@gmail.com>
2024-12-18 23:48:24 +0600

    error fix

--- app/components/how-to-buy/card.tsx
+++ app/components/how-to-buy/card.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface CardProps {
   title: string;
@@ -9,22 +9,40 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ title, description, subtitle }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Check window width after the component is mounted
+    if (typeof window !== 'undefined') {
+      setIsMobile(window.innerWidth <= 768);
+    }
+
+    // Optionally, listen for window resize
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <div
       className='buy-container relative lg:px-5 px-3 md:px-2 backdrop-blur-[3px] lg:max-w-[538px] md:h-[322px] md:max-w-[345px] h-[280px] overflow-hidden flex flex-col justify-center items-center lg:gap-4 gap-1 !bg-contain md:bg-cover z-50 md:pt-0 bg-center lazy-bg w-[93vw] bg-no-repeat pb-1 mx-8 md:mx-0'
       style={{
-        backgroundImage: 'url(/assets/buyframe.png)',
-        backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        background: window.innerWidth <= 768 ? 'none' : 'block',
+        backgroundImage: isMobile ? 'none' : 'url(/assets/buyframe.png)',
+        backgroundSize: isMobile ? 'none' : 'contain',
+        backgroundRepeat: isMobile ? 'none' : 'no-repeat',
+        backgroundPosition: isMobile ? 'none' : 'center',
       }}
     >
       <h1 className='lg:text-[25.36px] text-[22px] mt-4 lg:leading-[40px] leading-[30px] font-bold bg-gradient-to-b from-[#E4D9BD] to-[#AA8246] bg-clip-text text-transparent text-center'>
         {title}
       </h1>
       <p
-        className='text-center text-[#FFF1DE] mt-0 pt-2 px-3  text-wrap  font-medium 
+        className='text-center text-[#FFF1DE] mt-0 pt-2 px-4  text-wrap  font-medium 
              text-[15px] lg:leading-[21px] leading-[20px]
              lg:text-[17px]   
       '
