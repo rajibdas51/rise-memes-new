51134e1c0ab89ccb4e637c9680ec9dc4954339c9
Rajib Chandra Das <prorajib6@gmail.com>
2024-12-20 02:06:58 +0600

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,7 +92,15 @@ export default function RootLayout({
           sizes='16x16'
           href='/favicon-16x16.png'
         />
-        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='manifest' href='/site.webmanifest'></link>
+        <link rel='manifest' href='/manifest.json' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
+        <meta name='theme-color' content='#ffffff' />
+
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
+        <meta name='theme-color' content='#ffffff' />
 
         <meta
           name='viewport'
