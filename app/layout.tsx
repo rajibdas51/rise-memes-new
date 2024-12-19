24dc09865246973bfa1752abddb362295291bbe5
Rajib Chandra Das <prorajib6@gmail.com>
2024-12-20 01:38:30 +0600

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,10 +76,9 @@ export default function RootLayout({
     <html suppressHydrationWarning lang='en'>
       <head>
         <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
         />
         <link
           rel='icon'
@@ -90,14 +89,8 @@ export default function RootLayout({
         <link
           rel='icon'
           type='image/png'
-          sizes='512x512'
-          href='/large-favicon.png'
-        />
-        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
         />
         <link rel='manifest' href='/site.webmanifest' />
 
